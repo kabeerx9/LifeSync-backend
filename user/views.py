fc799c8b49ec627c629ef7f2c1790a81b8742640
kabeerx9 <kabeer786joshi@gmail.com>
2024-06-13 23:57:30 +0530

    JWT AUTH DONE SIMPLE VERSION - Final

--- user/views.py
+++ user/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -11,6 +11,7 @@ from .serializers import  RegistrationSerializer
 
 
 @api_view(['POST',])
+@permission_classes([AllowAny])
 def registration_view(request):
 
     if request.method == 'POST':
