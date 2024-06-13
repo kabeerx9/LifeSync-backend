0f094e899d439bcab2c8f9c620c933cd19190bfd
kabeerx9 <kabeer786joshi@gmail.com>
2024-06-13 15:03:27 +0530

    cors

--- lifesync_backend/settings.py
+++ lifesync_backend/settings.py
@@ -28,10 +28,18 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Replace with your Next.js frontend URL
+    "https://yourdomain.com",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders', 
     'rest_framework',
     'todos',
     'django.contrib.admin',
@@ -43,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
